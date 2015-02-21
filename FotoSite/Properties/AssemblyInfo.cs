2df71228298e66e974114ca99ce57283c8b893da
ak <ak@laminar.ru>
2015-02-21 15:38:20 +0500

    v1.0.1.21  - ExifTool v9.85

--- FotoSite/Properties/AssemblyInfo.cs
+++ FotoSite/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.20")]
-[assembly: AssemblyFileVersion("1.0.1.20")]
+[assembly: AssemblyVersion("1.0.1.21")]
+[assembly: AssemblyFileVersion("1.0.1.21")]
 
 // конфигурация лога из xml
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
